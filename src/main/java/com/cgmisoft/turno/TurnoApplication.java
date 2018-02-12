f795b60a6fdab2eb7383ef3470e5145f048aab4e
Maxi Bajo <achybajo@gmail.com>
2018-02-12 20:01:22 -0300

    initial commit

---
+++ src/main/java/com/cgmisoft/turno/TurnoApplication.java
@@ -0,0 +1,12 @@
+package com.cgmisoft.turno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurnoApplication.class, args);
+	}
+}
